c0b9f71801ca9a90a5d4f9bf5c916f9b94810ef2
minomushi-3855 <wngx5451@yahoo.co.jp>
2021-08-20 00:09:00 -0400

    country, languageとsourcesの併用が不可だということがわかったので修正

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="news-get",
+    package=["news", "news_auth"],
+    author="minomushi",
+    version="1.0.0",
+    install_requires=[
+        "newsapi-python"
+    ],
+    entry_points={
+        "console_scripts": [
+            "news = news.news:main"
+        ]
+    },
+    url="https://github.com/mino-38/news",
+    license="MIT",
+    keywords="NewsAPI news"
+)
